67396348adee3f538a030b93138010458dd3077e
XakerOrden <34403611+XakerOrden@users.noreply.github.com>
2018-10-25 08:41:05 +0300

    Add files via upload

---
+++ BlackZero/SDK/Globals.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackZero.SDK
+{
+    class Globals
+    {
+        public static bool bGlow = false;
+        public static bool bRadar = false;
+        public static bool bNoflash = false;
+        public static int iGlowMode;
+    }
+}
